17c76189f83872281944d9a8548ef41bcd1ffdf1
vladimirdevico1 <info@vladimirdevico.com>
2021-05-12 10:32:30 +0200

    Aggiunto model e connession DB

---
+++ lib/models/note.dart
@@ -0,0 +1,11 @@
+class Note {
+  final int id;
+  final string title;
+  final string content;
+
+  Note(this.id, this.title, this.content);
+
+  // Note([this.id, this.title, this.content]); Opzionali ma in fila
+  // Note({this.id, this.title, this.content}); Opzionali ma in richiamabili con nominativo
+
+}
